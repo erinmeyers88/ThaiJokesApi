e5f9d3072345d356aee637ac00cbaab6557f0eba
Vincent, Erin <evincent@ariit.com>
2018-08-02 10:35:01 -0600

    build

---
+++ build/db.js
@@ -0,0 +1,4 @@
+export default {
+  url: 'mongodb://test:test12@ds111082.mlab.com:11082/thai-jokes'
+};
+//# sourceMappingURL=db.js.map
---
+++ build/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import { MongoClient } from 'mongodb';
+import bodyParser from 'body-parser';
+import routes from './routes';
+import db from './db';
+
+const app = express();
+const port = 8000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err);
+
+  const db = database.db("thai-jokes");
+
+  routes(app, db);
+  app.listen(port, () => {
+    console.log('Listening on port ', port);
+  });
+});
+
+//https://www.dek-d.com/board/view/2326563/
+//# sourceMappingURL=index.js.map
