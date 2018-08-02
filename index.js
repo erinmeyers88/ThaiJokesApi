39bb66ed723380439a771696171cdc1f8789865e
Vincent, Erin <evincent@ariit.com>
2018-08-02 10:19:22 -0600

    Thai Jokes API

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import {MongoClient} from 'mongodb';
+import bodyParser from 'body-parser';
+import routes from './routes';
+import db from './db';
+
+const app = express();
+const port = 8000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err);
+
+  const db = database.db("test");
+
+  routes(app, db);
+  app.listen(port, () => {
+    console.log('Listening on port ', port);
+  });
+
+});
+
+//https://www.dek-d.com/board/view/2326563/
+
